c29ea91f525329962829ae8d13a337a147410369
vtdn91 <vt.dn91@gmail.com>
2016-12-20 06:37:23 +0900

    Initial version

---
+++ mx6qsabresd_config/include/generated/timestamp_autogenerated.h
@@ -0,0 +1,2 @@
+#define U_BOOT_DATE "Dec 02 2016"
+#define U_BOOT_TIME "05:30:18"
---
+++ mx6qsabresd_config/include/generated/version_autogenerated.h
@@ -0,0 +1,4 @@
+#define PLAIN_VERSION "2015.04imx_v2015.04_4.1.15_1.2.0_ga+gede7538"
+#define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
+#define CC_VERSION_STRING "arm-poky-linux-gnueabi-gcc (GCC) 5.2.0"
+#define LD_VERSION_STRING "GNU ld (GNU Binutils) 2.25.1"
